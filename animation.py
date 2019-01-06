71d59aa473c597703ced8a8b66ed8d4b4be7fed5
s-light <github@s-light.eu>
2019-01-06 19:47:15 +0100

    animation.py seems to work :-)

--- animation.py
+++ animation.py
@@ -12,6 +12,7 @@ import time
 
 import board
 import busio
+import supervisor
 
 from adafruit_tlc59711.adafruit_tlc59711_multi import TLC59711Multi
 import adafruit_fancyled.adafruit_fancyled as fancyled
@@ -30,91 +31,74 @@ if __name__ == '__main__':
 print(42 * '*')
 print("define pixel array / init TLC5971")
 
-LEDBoard_count = 8
-# LEDBoard_count = 1
-LEDBoard_row_count = 4
+##########################################
+# mappings
+# LEDBoard_4x4_16bit mapping
+
 LEDBoard_col_count = 4
-pixel_col_count = LEDBoard_count * LEDBoard_col_count
-pixel_count = pixel_col_count * LEDBoard_row_count
+LEDBoard_row_count = 4
+LEDBoard_pixel_count = LEDBoard_row_count * LEDBoard_col_count
+LEDBoard_single = [
+    [15, 14, 11, 10],
+    [13, 12, 9, 8],
+    [7, 6, 3, 2],
+    [5, 4, 1, 0],
+]
+
+Boards_col_count = 2
+Boards_row_count = 4
+Boards_count = Boards_col_count * Boards_row_count
+Boards_positions = [
+    [4, 0],
+    [5, 1],
+    [6, 2],
+    [7, 3],
+]
+
+Matrix_col_count = LEDBoard_col_count * Boards_col_count
+Matrix_row_count = LEDBoard_row_count * Boards_row_count
+Matrix_pixel_count = Matrix_col_count * Matrix_row_count
+
+
+def mymap_LEDBoard_4x4_16bit(self, *, col=0, row=0):
+    """Map row and col to pixel_index."""
+    # get Board position
+    board_col = col // LEDBoard_col_count
+    board_row = row // LEDBoard_row_count
+    board_sub_col = col % LEDBoard_col_count
+    board_sub_row = row % LEDBoard_row_count
+
+    board_offset = Boards_positions[board_row][board_col]
+    pixel_offset = LEDBoard_single[board_sub_row][board_sub_col]
+
+    pixel_index = (board_offset * LEDBoard_pixel_count) + pixel_offset
+
+    return pixel_index
+
+
+pmap = PixelMap2D(
+    row_count=Matrix_row_count,
+    col_count=Matrix_col_count,
+    map_function=mymap_LEDBoard_4x4_16bit
+)
+
+##########################################
+# led controller
 
 spi = busio.SPI(board.SCK, MOSI=board.MOSI)
-pixels = TLC59711Multi(spi, pixel_count=pixel_count)
+pixels = TLC59711Multi(spi, pixel_count=Matrix_pixel_count)
 
 
-# How to map a 2D-Pixel Array to the needed TLC5971 sending order:
-
-
-# create empty 2d list
-# this will be filled by the initialisation for all chips/pcbs
-pixel_map = [
-    [0 for c in range(pixel_col_count)] for r in range(LEDBoard_row_count)
+##########################################
+# Declare a 6-element RGB rainbow palette
+palette = [
+    fancyled.CRGB(1.0, 0.0, 0.0),  # Red
+    fancyled.CRGB(0.5, 0.5, 0.0),  # Yellow
+    fancyled.CRGB(0.0, 1.0, 0.0),  # Green
+    fancyled.CRGB(0.0, 0.5, 0.5),  # Cyan
+    fancyled.CRGB(0.0, 0.0, 1.0),  # Blue
+    fancyled.CRGB(0.5, 0.0, 0.5),  # Magenta
 ]
-# print(pixel_map)
-
-
-def pixel_map_fill():
-    """."""
-    index_offset = 0
-
-    print(
-        "|----------index----------|-----------map----------|\n"
-        "| board | row | col | cwb | offset | board | pixel |"
-    )
-    for board_index in range(LEDBoard_count):
-        for row_index in range(LEDBoard_row_count):
-            for col_index in range(LEDBoard_col_count):
-                col_index_w_board = col_index * board_index
-                # set new pixel_index
-                pixel_map[row_index][col_index_w_board] = (
-                    index_offset + LEDBoard_single[row_index][col_index])
-                # print debug things
-                print(
-                    "| "
-                    "{board:^5} | "
-                    "{row:^3} | "
-                    "{col:^3} | "
-                    "{col_w_board:^3} | "
-                    "{index_offset:^6} | "
-                    "{board_map:^5} | "
-                    "{pixel_map:^5} | "
-                    "".format(
-                        board=board_index,
-                        col=col_index,
-                        row=row_index,
-                        col_w_board=col_index_w_board,
-                        index_offset=index_offset,
-                        board_map=LEDBoard_single[row_index][col_index],
-                        pixel_map=pixel_map[row_index][col_index_w_board],
-                    )
-                )
-
-        # prepare next board
-        index_offset += pixels.CHIP_LED_COUNT
-    print("|--------------------------------------------------|")
-
-# pixel_map_fill()
-
-
-def pixel_map_print_information():
-    """Print all Information regarding pixel_map."""
-    print(
-        "Pixels & Mapping:\n"
-        "  LEDBoard_count {LEDBoard_count:>2}\n"
-        "  LEDBoard_row_count           {LEDBoard_row_count:>2}\n"
-        "  LEDBoard_col_count           {LEDBoard_col_count:>2}\n"
-        "  pixel_count    {pixel_count:>2}\n"
-        "  chip_count     {chip_count:>2}\n"
-        "  channel_count  {channel_count:>2}\n"
-        "".format(
-            LEDBoard_count=LEDBoard_count,
-            LEDBoard_row_count=LEDBoard_row_count,
-            LEDBoard_col_count=LEDBoard_col_count,
-            pixel_count=pixels.pixel_count,
-            chip_count=pixels.chip_count,
-            channel_count=pixels.channel_count,
-        )
-    )
-    print(pixel_map)
 
 
 ##########################################
@@ -134,159 +118,222 @@ def map_range_int(x, in_min, in_max, out_min, out_max):
     )
 
 
-##########################################
-# mapping function
-
-def get_pixel_index_from_row_col(row, col):
-    """Get pixel_index from row and column index."""
-    pixel_index = pixel_map[row][col]
-    return pixel_index
-
-
-##########################################
-# Declare a 6-element RGB rainbow palette
-palette = [
-    fancyled.CRGB(1.0, 0.0, 0.0),  # Red
-    fancyled.CRGB(0.5, 0.5, 0.0),  # Yellow
-    fancyled.CRGB(0.0, 1.0, 0.0),  # Green
-    fancyled.CRGB(0.0, 0.5, 0.5),  # Cyan
-    fancyled.CRGB(0.0, 0.0, 1.0),  # Blue
-    fancyled.CRGB(0.5, 0.0, 0.5),  # Magenta
-]
+def wait_with_print(duration=1):
+    """Wait with print."""
+    step_duration = 0.5
+    waiting_duration = 0
+    while waiting_duration < duration:
+        # print(". ", end='', flush=True)
+        print(".", end='')
+        time.sleep(step_duration)
+        waiting_duration += step_duration
+    print("")
 
 
 ##########################################
-# mappings
 
-LEDBoard_row_count = 4
-LEDBoard_col_count = 4
-LEDBoard_pixel_count = LEDBoard_row_count * LEDBoard_col_count
-LEDBoard_single = [
-    [4, 5, 12, 13],
-    [6, 7, 14, 15],
-    [0, 1, 8, 11],
-    [2, 3, 10, 12],
-]
+class AnimationHelper(object):
+    """AnimationHelper."""
 
-Boards_row_count = 4
-Boards_col_count = 2
-Boards_positions = [
-    [4, 8],
-    [3, 7],
-    [2, 6],
-    [1, 5],
-]
+    def __init__(self):
+        """Init."""
+        super(AnimationHelper, self).__init__()
+        self.offset = 0
+        self.animation_run = True
 
-matrix_col_count = LEDBoard_col_count * Boards_col_count
-matrix_row_count = LEDBoard_row_count * Boards_row_count
+    ##########################################
+    # test functions
 
-
-def mymap_LEDBoard_4x4_16bit(self, row, col):
-    """Map row and col to pixel_index."""
-
-    Board_count = self.pixel_count // LEDBoard_pixel_count
-
-    # physical_index = (row * self.col_count) + col
-
-    # row_inv = (self.row_count-1) - row
-    col_inv = (self.col_count - 1) - col
-
-    # split chip position from chip_inner
-    chipin_row = row % LEDBoard_row_count
-    chipin_col = col % LEDBoard_col_count
-    # chip_row_inv = row_inv // LEDBoard_row_count
-    chip_col_inv = col_inv // LEDBoard_col_count
-
-    chipin_row_inv = (LEDBoard_row_count - 1) - chipin_row
-    chipin_col_inv = (LEDBoard_col_count - 1) - chipin_col
-    chipin_pixel_index = (
-        (chipin_row_inv * LEDBoard_col_count) + chipin_col_inv
-    )
-
-    pixel_index = (chipin_pixel_index * Board_count) + chip_col_inv
-
-    return pixel_index
-
-##########################################
-# test functions
-
-def test_set_corners_to_colors():
-    """Test Function: Set all 4 corners to different collors."""
-    print(42 * '*')
-    print("set corners to colors")
-    pixels[get_pixel_index_from_row_col(0, 0)] = (1.0, 0.5, 0.0)
-    pixels[get_pixel_index_from_row_col(0, 3)] = (0.5, 0.0, 1.0)
-    pixels[get_pixel_index_from_row_col(3, 0)] = (0.1, 0.5, 0.0)
-    pixels[get_pixel_index_from_row_col(3, 3)] = (0.0, 0.5, 1.0)
-    pixels.show()
-
-
-def test_set_2d_colors():
-    """Test Function: Set all LEDs to 2D color-range."""
-    print("set color range")
-    for x in range(LEDBoard_col_count):
-        # xN = x / LEDBoard_col_count
-        xN = map_range_int(x, 0, LEDBoard_col_count, 1, 100)
-        for y in range(LEDBoard_row_count):
-            # yN = y / LEDBoard_row_count
-            yN = map_range_int(y, 0, LEDBoard_row_count, 1, 100)
-            # print(
-            #     "x: {:>2} xN: {:>2} "
-            #     "y: {:>2} yN: {:>2} "
-            #     "pixel_index: {:>2}".format(
-            #         x, xN,
-            #         y, yN,
-            #         get_pixel_index_from_row_col(x, y)
-            #     )
-            # )
-            pixels[get_pixel_index_from_row_col(x, y)] = (xN, yN, 0)
-
-    pixels.show()
-
-
-def test_loop_2d_colors():
-    """Test Function: Set all LEDs to 2D color-range."""
-    # Positional offset for blue part
-    offset = 0
-    print(42 * '*')
-    print("loop")
-    while True:
-        offsetN = map_range_int(offset, 0.0, 1.0, 1, 200)
-        for x in range(LEDBoard_col_count):
-            xN = map_range_int(x, 0, LEDBoard_col_count, 1, 500)
-            for y in range(LEDBoard_row_count):
-                yN = map_range_int(y, 0, LEDBoard_row_count, 1, 500)
-                pixels[get_pixel_index_from_row_col(x, y)] = (xN, yN, offsetN)
+    @staticmethod
+    def test_set_corners_to_colors():
+        """Test Function: Set all 4 corners to different collors."""
+        print(42 * '*')
+        print("set corners to colors")
+        pixels[pmap.map(
+            col=0,
+            row=0
+        )] = (0.2, 0.05, 0.0)
+        pixels[pmap.map(
+            col=0,
+            row=Matrix_row_count-1
+        )] = (0.1, 0.0, 0.2)
+        pixels[pmap.map(
+            col=Matrix_col_count-1,
+            row=0
+        )] = (0.1, 0.2, 0.0)
+        pixels[pmap.map(
+            col=Matrix_col_count-1,
+            row=Matrix_row_count-1
+        )] = (0.0, 0.1, 0.2)
+        # print("{:>3}:{:>3} = {:>3}".format(0, 0, pmap.map(0, 0)))
+        # print("{:>3}:{:>3} = {:>3}".format(0, 7, pmap.map(0, 7)))
+        # print("{:>3}:{:>3} = {:>3}".format(15, 0, pmap.map(15, 0)))
+        # print("{:>3}:{:>3} = {:>3}".format(15, 7, pmap.map(15, 7)))
+        # pixels[pmap.map(0, 0)] = (0.1, 0.01, 0.0)
+        # pixels[pmap.map(0, 7)] = (0.01, 0.0, 0.1)
+        # pixels[pmap.map(15, 0)] = (0.01, 0.01, 0.0)
+        # pixels[pmap.map(15, 7)] = (0.0, 0.01, 0.1)
         pixels.show()
-        offset += 0.01  # Bigger number = faster spin
-        if offset > 1.0:
-            offset = 0
+
+    @staticmethod
+    def test_set_2d_colors():
+        """Test Function: Set all LEDs to 2D color-range."""
+        print("set color range")
+        for x in range(Matrix_col_count):
+            # xN = x / Matrix_col_count
+            xN = map_range_int(x, 0, Matrix_col_count, 1, 500)
+            for y in range(Matrix_row_count):
+                # yN = y / Matrix_row_count
+                yN = map_range_int(y, 0, Matrix_row_count, 1, 500)
+                # print(
+                #     "x: {:>2} xN: {:>2} "
+                #     "y: {:>2} yN: {:>2} "
+                #     "pixel_index: {:>2}".format(
+                #         x, xN,
+                #         y, yN,
+                #         get_pixel_index_from_row_col(x, y)
+                #     )
+                # )
+                pixel_index = 0
+                try:
+                    pixel_index = pmap.map(col=x, row=y)
+                except IndexError as e:
+                    print("{}; col:{col} row:{row}".format(e, col=x, row=y))
+                pixels[pixel_index] = (xN, yN, 0)
+
+        pixels.show()
+
+    def test_loop_2d_colors(self):
+        """Test Function: Set all LEDs to 2D color-range."""
+        # Positional offset for blue part
+        offsetN = map_range_int(self.offset, 0.0, 1.0, 1, 1000)
+        for x in range(Matrix_col_count):
+            xN = map_range_int(x, 0, Matrix_col_count, 1, 2000)
+            for y in range(Matrix_row_count):
+                yN = map_range_int(y, 0, Matrix_row_count, 1, 2000)
+                pixels[pmap.map(col=x, row=y)] = (xN, yN, offsetN)
+        pixels.show()
+        self.offset += 0.001  # Bigger number = faster spin
+        if self.offset > 1.0:
+            self.offset = 0
+
+    def rainbow_update(self):
+        """Rainbow."""
+        for row_index in range(Matrix_row_count):
+            # Load each pixel's color from the palette using an offset, run it
+            # through the gamma function, pack RGB value and assign to pixel.
+            color = fancyled.palette_lookup(
+                palette,
+                self.offset + row_index / pixels.pixel_count)
+            color = fancyled.gamma_adjust(color, brightness=0.25)
+            # print("{index:>2} : {div:>2} | {mod:>2}".format(
+            #     index=i,
+            #     div=i // 4,
+            #     mod=i % 4
+            # ))
+            for col_index in range(Matrix_col_count):
+                pixels[pmap.map(col=col_index, row=row_index)] = color
+        pixels.show()
+
+        self.offset += 0.001  # Bigger number = faster spin
+        if self.offset >= 10:
+            self.offset -= 10
+
+    @staticmethod
+    def handle_pixel_set(input_string):
+        """Handle Pixel Set."""
+        pixel = 0
+        value = 0
+        sep = input_string.find(":")
+        try:
+            pixel = int(input_string[1:sep])
+        except ValueError as e:
+            print("Exception parsing 'pixel': ", e)
+        try:
+            value = int(input_string[sep+1:])
+        except ValueError as e:
+            print("Exception parsing 'value': ", e)
+        pixels.set_pixel_16bit_value(pixel, value, value, value)
+        pixels.show()
+
+    @staticmethod
+    def handle_pixel_map_set(input_string):
+        """Handle Pixel Set."""
+        row = 0
+        col = 0
+        value = 0
+
+        sep_pos = input_string.find(",")
+        sep_value = input_string.find(":")
+        try:
+            col = int(input_string[1:sep_pos])
+        except ValueError as e:
+            print("Exception parsing 'col': ", e)
+        try:
+            row = int(input_string[sep_pos+1:sep_value])
+        except ValueError as e:
+            print("Exception parsing 'row': ", e)
+        try:
+            value = int(input_string[sep_value+1:])
+        except ValueError as e:
+            print("Exception parsing 'value': ", e)
+        pixel_index = 0
+        try:
+            pixel_index = pmap.map(col=col, row=row)
+        except IndexError as e:
+            print("{}; col:'{:>3}' row:'{:>3}'".format(e, col, row))
+
+        print(
+            "pixel_index:'{:>3}' col:'{:>3}' row:'{:>3}'"
+            "".format(pixel_index, col, row)
+        )
+        pixels.set_pixel_16bit_value(pixel_index, value, value, value)
+        pixels.show()
+
+    def check_input(self):
+        """Check Input."""
+        new_value = input()
+        if "p" in new_value:
+            self.handle_pixel_set(new_value)
+        if "m" in new_value:
+            self.handle_pixel_map_set(new_value)
+        if "a" in new_value:
+            self.animation_run = not self.animation_run
+        # prepare new input
+        # print("enter new values:")
+        print(">>", end="")
+
+    def run_test(self):
+        """Test Main."""
+        pmap.print_mapping()
+
+        pixels.set_pixel_all_16bit_value(1, 1, 1)
+        pixels.show()
+
+        self.test_set_corners_to_colors()
+        wait_with_print(1)
+        self.test_set_2d_colors()
+        wait_with_print(1)
+
+        print(42 * '*')
+        print("loop")
+        if supervisor.runtime.serial_connected:
+            print(
+                "you can set a single pixel manually:\n"
+                "example: 'p18:500' or 'm2,5:500'"
+            )
+        while True:
+            if supervisor.runtime.serial_bytes_available:
+                self.check_input()
+            if self.animation_run:
+                # self.test_loop_2d_colors()
+                self.rainbow_update()
 
 
-def test_main():
-    """Test Main."""
-    test_set_corners_to_colors()
-    time.sleep(10)
-    test_set_2d_colors()
-    time.sleep(10)
-    test_loop_2d_colors()
-
-
-def test_map():
-    """Test Map."""
-    #####################
-    # LEDBoard_4x4_16bit mapping
-
-    pmap = PixelMap2D(
-        row_count=matrix_row_count,
-        col_count=matrix_col_count,
-        map_function=mymap_LEDBoard_4x4_16bit)
-    pmap.print_mapping()
-
+animation_helper = AnimationHelper()
 
 ##########################################
 # main loop
 
 if __name__ == '__main__':
-    # test_main()
-    test_map()
+    animation_helper.run_test()
